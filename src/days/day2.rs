ee05da5461116feb5ae47c6703be71444a3453da
Ede Meijer <ede.meijer88@gmail.com>
2018-12-03 11:12:33 +0100

    Day 2 part 1

---
+++ src/days/day2.rs
@@ -0,0 +1,80 @@
+use std::collections::HashMap;
+use std::collections::HashSet;
+
+use utils::load_data;
+use utils::non_empty_lines;
+
+#[allow(dead_code)]
+pub fn part1() {
+    println!("{}", solve_part1(get_puzzle_input()));
+}
+
+fn solve_part1(box_ids: Vec<String>) -> u32 {
+    // Track how many times different kinds of multiples occur in box IDs
+    let mut multiple_counts = HashMap::new();
+
+    for id in box_ids {
+        // Collect the unique multiples of letters in a hash set
+        let unique_counts = count_letters(id)
+            .into_iter()
+            .map(|(_, count)| count)
+            .collect::<HashSet<u32>>();
+
+        // Use them to update the numbers of multiple occurrences
+        for count in unique_counts {
+            *multiple_counts.entry(count).or_insert(0u32) += 1;
+        }
+    }
+
+    // Use pattern matching to multiply the occurrences if they both exist, otherwise return 0
+    match (multiple_counts.get(&2), multiple_counts.get(&3)) {
+        (Some(a), Some(b)) => a * b,
+        _ => 0
+    }
+}
+
+fn count_letters(id: String) -> HashMap<char, u32> {
+    let mut counts = HashMap::new();
+    for letter in id.chars() {
+        *counts.entry(letter).or_insert(0) += 1;
+    }
+    counts
+}
+
+fn get_puzzle_input() -> Vec<String> {
+    non_empty_lines(load_data("day2"))
+}
+
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_count_letters() {
+        assert_eq!(
+            count_letters(String::from("abbccccb")),
+            hashmap! {
+                'a' => 1,
+                'b' => 3,
+                'c' => 4,
+            }
+        )
+    }
+
+    #[test]
+    fn test_part1() {
+        assert_eq!(
+            solve_part1(vec![
+                String::from("abcdef"),
+                String::from("bababc"),
+                String::from("abbcde"),
+                String::from("abcccd"),
+                String::from("aabcdd"),
+                String::from("abcdee"),
+                String::from("ababab"),
+            ]),
+            12
+        )
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+#[macro_use]
+#[cfg(test)]
+extern crate maplit;
+
 use days::*;
 
 mod days;
@@ -5,5 +9,6 @@ pub mod utils;
 
 fn main() {
 //    day1::part1();
-    day1::part2();
+//    day1::part2();
+    day2::part1();
 }
