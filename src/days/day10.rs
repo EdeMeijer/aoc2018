a1f300519a26140d3f94422cf2566e33e126ca6e
Ede Meijer <ede.meijer88@gmail.com>
2018-12-11 09:09:34 +0100

    Use closed form solution for day 10

--- src/days/day10.rs
+++ src/days/day10.rs
@@ -1,8 +1,8 @@
 //! Solutions for https://adventofcode.com/2018/day/10
-
-use utils::data::non_empty_lines;
-use utils::data::load_data;
 use regex::Regex;
+
+use utils::data::load_data;
+use utils::data::non_empty_lines;
 use utils::matrix::Matrix;
 
 #[allow(dead_code)]
@@ -17,30 +17,21 @@ pub fn part2() {
 
 struct Point {
     p: (i32, i32),
-    v: (i32, i32)
+    v: (i32, i32),
 }
 
-fn solve_both_parts(points: Vec<Point>) -> (String, usize) {
-    let mut area = area_of(&points);
-    let mut t = 0;
+fn solve_both_parts(p: Vec<Point>) -> (String, usize) {
+    let mu = mean(p.iter().map(|p| p.p.0 as f32)).unwrap();
+    let ev = mean(p.iter().map(|p| p.v.0 as f32)).unwrap();
+    let t = mean(p.iter().map(|p| (mu - p.p.0 as f32) / (p.v.0 as f32 - ev)))
+        .unwrap().round() as i32;
 
-    loop {
-        t += 1;
-        let new_points = project(&points, t);
-        let new_area = area_of(&new_points);
-
-        if new_area < area {
-            area = new_area;
-        } else {
-            t -= 1;
-            break (format_points(&project(&points, t)), t as usize)
-        }
-    }
+    (format_points(&project(&p, t)), t as usize)
 }
 
-fn area_of(points: &Vec<Point>) -> i64 {
-    let ((x_min, x_max), (y_min, y_max)) = get_aabb(&points);
-    (y_max - y_min) as i64 * (x_max - x_min) as i64
+fn mean(iter: impl Iterator<Item=f32>) -> Option<f32> {
+    let (sum, count) = iter.fold((0.0, 0), |(s, c), v| (s + v, c + 1));
+    if count > 0 { Some(sum / count as f32) } else { None }
 }
 
 fn project(points: &Vec<Point>, time: i32) -> Vec<Point> {
@@ -48,7 +39,7 @@ fn project(points: &Vec<Point>, time: i32) -> Vec<Point> {
         .map(|point| {
             Point {
                 p: (point.p.0 + point.v.0 * time, point.p.1 + point.v.1 * time),
-                v: point.v
+                v: point.v,
             }
         })
         .collect()
@@ -60,12 +51,12 @@ fn format_points(points: &Vec<Point>) -> String {
     let mut grid = Matrix::new(
         (y_max - y_min + 1) as usize,
         (x_max - x_min + 1) as usize,
-        '.'
+        '.',
     );
     for point in points {
         grid[((point.p.1 - y_min) as usize, (point.p.0 - x_min) as usize)] = '#';
     }
-    
+
     let result = grid.rows()
         .map(|row| {
             let row = row.iter()
@@ -74,7 +65,7 @@ fn format_points(points: &Vec<Point>) -> String {
             format!("{}\n", row)
         })
         .collect::<String>();
-    
+
     result.trim().to_string()
 }
 
@@ -114,7 +105,7 @@ fn parse_input_line(line: String) -> Point {
 #[cfg(test)]
 mod test {
     use super::*;
-    
+
     #[test]
     fn test_solve() {
         let expected = String::from("
@@ -126,13 +117,13 @@ mod test {
 #...#...#.
 #...#...#.
 #...#..###".trim());
-        
+
         assert_eq!(
             solve_both_parts(parse_input(get_test_data())),
             (expected, 3)
         );
     }
-    
+
     fn get_test_data() -> String {
         String::from("
 position=< 9,  1> velocity=< 0,  2>
